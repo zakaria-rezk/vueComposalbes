23ecabeaf23a330b1c9c7e974276e5072888141d
zekor <zakariarezk0@gmail.com>
2024-04-30 13:09:22 +0300

    first commit

---
+++ src/components/AddUser.vue
@@ -0,0 +1,20 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+    <p>Do you want to continue with adding a user?</p>
+  </user-alert>
+  <section>
+    <h2>Add a User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script setup>
+
+import usealert from '../hooks/alert';
+import UserAlert from '@/components/UserAlert'
+const {alertIsVisible,showAlert,hideAlert} = usealert()
+
+
+
+
+</script>
---
+++ src/components/DeleteUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script setup>
+import usealert from '../hooks/alert';
+import UserAlert from '@/components/UserAlert'
+const {alertIsVisible,showAlert,hideAlert} = usealert()
+
+
+</script>
